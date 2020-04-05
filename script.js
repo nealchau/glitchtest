86656414546dbd66000269b0dfb7ee64f28df869
Glitch (kiwi-destiny-spot) <none>
2020-04-05 20:54:02 +0000

    💵⛳️ Updated with Glitch

---
+++ script.js
@@ -0,0 +1,7 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+document.getElementById("demo").innerHTML = 5 + 6;
+
+// prints "hi" in the browser's dev tools console
+console.log("hi");
